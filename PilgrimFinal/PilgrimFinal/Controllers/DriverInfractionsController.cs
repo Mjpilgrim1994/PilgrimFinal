0e844ac98e7d3114997dc9778d53b865a63a774c
Mjpilgrim1994 <mattpilgrim1994@gmail.com>
2023-05-01 14:31:43 -0500

    CRUD ops

---
+++ PilgrimFinal/PilgrimFinal/Controllers/DriverInfractionsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PilgrimFinal.Data;
+using PilgrimFinal.Models;
+
+namespace PilgrimFinal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriverInfractionsController : ControllerBase
+    {
+        private readonly pilgrim_dmvContext _context;
+
+        public DriverInfractionsController(pilgrim_dmvContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/DriverInfractions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DriverInfraction>>> GetDriverInfractions()
+        {
+          if (_context.DriverInfractions == null)
+          {
+              return NotFound();
+          }
+            return await _context.DriverInfractions.ToListAsync();
+        }
+
+        // GET: api/DriverInfractions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DriverInfraction>> GetDriverInfraction(int id)
+        {
+          if (_context.DriverInfractions == null)
+          {
+              return NotFound();
+          }
+            var driverInfraction = await _context.DriverInfractions.FindAsync(id);
+
+            if (driverInfraction == null)
+            {
+                return NotFound();
+            }
+
+            return driverInfraction;
+        }
+
+        // PUT: api/DriverInfractions/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDriverInfraction(int id, DriverInfraction driverInfraction)
+        {
+            if (id != driverInfraction.InfractionId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(driverInfraction).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DriverInfractionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/DriverInfractions
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<DriverInfraction>> PostDriverInfraction(DriverInfraction driverInfraction)
+        {
+          if (_context.DriverInfractions == null)
+          {
+              return Problem("Entity set 'pilgrim_dmvContext.DriverInfractions'  is null.");
+          }
+            _context.DriverInfractions.Add(driverInfraction);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDriverInfraction", new { id = driverInfraction.InfractionId }, driverInfraction);
+        }
+
+        // DELETE: api/DriverInfractions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDriverInfraction(int id)
+        {
+            if (_context.DriverInfractions == null)
+            {
+                return NotFound();
+            }
+            var driverInfraction = await _context.DriverInfractions.FindAsync(id);
+            if (driverInfraction == null)
+            {
+                return NotFound();
+            }
+
+            _context.DriverInfractions.Remove(driverInfraction);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DriverInfractionExists(int id)
+        {
+            return (_context.DriverInfractions?.Any(e => e.InfractionId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ PilgrimFinal/PilgrimFinal/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using PilgrimFinal.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<pilgrim_dmvContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("PilgrimFinal"));
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
