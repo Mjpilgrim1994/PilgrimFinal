0c55c2f9ed6cbb7ed3298503f9ab1fc7540047d7
Mjpilgrim1994 <mattpilgrim1994@gmail.com>
2023-05-05 16:28:30 -0500

    Dictionary

---
+++ DictionaryProblem/DictionaryProblem/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Generating the List
+        List<KeyValuePair<string, string>> list = new List<KeyValuePair<string, string>>()
+{
+    new KeyValuePair<string, string>("doge", "coin"),
+    new KeyValuePair<string, string>("abc", "frqscn"),
+    new KeyValuePair<string, string>("12k", "qrs"),
+    new KeyValuePair<string, string>("doge", "coin"),
+    new KeyValuePair<string, string>("farmnadc", "qrescs"),
+    new KeyValuePair<string, string>("gqers", "ddsqerd"),
+    new KeyValuePair<string, string>("abc", "mndsa"),
+    new KeyValuePair<string, string>("volkawagen", "Jetta TDI"),
+    new KeyValuePair<string, string>("doge", "coin"),
+    new KeyValuePair<string, string>("doge", "frqscn")
+};
+
+        Dictionary<string, string> mainDictionary = new Dictionary<string, string>();
+        Dictionary<string, int> countDictionary = new Dictionary<string, int>();
+
+        // Stepping though kvp list
+        foreach (KeyValuePair<string, string> pair in list)
+        {
+            // Searching for duplicate values in the list
+            if (mainDictionary.ContainsKey(pair.Key))
+            {
+                // 
+                if (!countDictionary.ContainsKey(pair.Key))
+                {
+                    int value = 2;
+                    countDictionary.Add(pair.Key, value);
+                }
+                // 
+                else
+                {
+                    countDictionary.TryGetValue(pair.Key, out int value);
+                    int count = value;
+
+                    countDictionary[pair.Key] = ++count;
+                }
+            }
+            // Add each unique list item to the main dictionary
+            else
+            {
+                mainDictionary.Add(pair.Key, pair.Value);
+            }
+        }
+
+        // Printing the keys and their values from main dictionary
+        Console.WriteLine("--- Writing unique values ---");
+        foreach (var kvp in mainDictionary)
+        {
+            Console.WriteLine($"Key: {kvp.Key} with Value: {kvp.Value}");
+        }
+
+        // Printing each kvp that are duplicates and their counts
+        Console.WriteLine("--- Writing count details ---");
+        foreach (var kvp in countDictionary)
+        {
+            Console.WriteLine($"Key: {kvp.Key} with Count: {kvp.Value}");
+        }
+    }
+}
---
+++ PilgrimFinalDmv/PilgrimFinalDmv/DictionaryProblem.cs
@@ -0,0 +1,17 @@
+﻿internal class DictionaryProblem
+{
+    private static void Main(string[] args)
+    {
+        Dictionary<string, string> mainDictionary = new Dictionary<string, string>();
+        mainDictionary.Add("a", "abs");
+        mainDictionary.Add("b", "basic");
+        mainDictionary.Add("v", "venus");
+        mainDictionary.Add("d", "dentist");
+
+
+        foreach (KeyValuePair<string, string> kvp in mainDictionary)
+        {
+            Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+        }
+    }
+}
